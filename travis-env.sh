a4735afa6a5fec380adbdc2e23065286e82c8979
Crystal E Chun <Crystal.Chun@ibm.com>
2019-10-25 12:45:30 -0400

    Update travis-env.sh
    Removed hard-coded versioning

--- travis-env.sh
+++ travis-env.sh
@@ -2,11 +2,8 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     if ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
         ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
-        VERSION=99.99.99
-        export VERSION_NUMBER="$VERSION"
         export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
     fi
 fi
 
 echo ARTIFACTORY_REPO=$ARTIFACTORY_REPO
-echo VERSION=$VERSION_NUMBER
