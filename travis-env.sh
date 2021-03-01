eff2da73e854dc4462a798dc1367b114716e6abe
Gus Parvin <gparvin@redhat.com>
2021-03-01 14:56:39 -0500

    switching master to main

--- travis-env.sh
+++ travis-env.sh
@@ -1,5 +1,5 @@
 # Release Tag and Repository
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "main" ]; then
     if ! [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
         ARTIFACTORY_REPO=hyc-cloud-private-integration-helm-local
         export ARTIFACTORY_REPO="$ARTIFACTORY_REPO"
